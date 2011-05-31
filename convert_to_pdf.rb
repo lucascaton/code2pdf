98c6234d86f5b414f40b92aa203cf99254b0376c
Lucas Caton <lucascaton@gmail.com>
2011-05-31 19:45:08 -0300

    PDF conversion

---
+++ convert_to_pdf.rb
@@ -0,0 +1,27 @@
+class ConvertToPDF
+
+  PDF_OPTIONS = {
+    :page_size   => 'A4'
+  }
+
+  def initialize(filename, code_files)
+    @filename, @code_files = filename, code_files
+  end
+
+  def pdf
+    Prawn::Document.new PDF_OPTIONS do |pdf|
+      @code_files.each do |file|
+        pdf.font 'Courier' do
+          pdf.text file.first, :size => 12, :inline_format => true
+          pdf.move_down 20
+          pdf.text file.last, :font => 'Monaco', :size => 12, :inline_format => true
+          pdf.move_down 40
+        end
+      end
+    end
+  end
+
+  def save
+    pdf.render_file @filename
+  end
+end
