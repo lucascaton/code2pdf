8a3d21c51f04e51e8ca6afd1e7a3d04206fb7049
Lucas Caton <lucascaton@gmail.com>
2016-09-24 10:09:38 +1000

    Clean up code

--- lib/code2pdf/convert_to_pdf.rb
+++ lib/code2pdf/convert_to_pdf.rb
@@ -1,21 +1,23 @@
 class ConvertToPDF
   PDF_OPTIONS = {
-    :page_size   => 'A4'
+    :page_size => 'A4'
   }
 
   def initialize(params={})
-    if not params.has_key?(:from) or params[:from].nil?
-      raise ArgumentError.new 'where are the codes you want to convert to pdf?'
+    if not params.has_key?(:from) || params[:from].nil?
+      raise ArgumentError.new 'where is the codebase you want to convert to PDF?'
     elsif not valid_directory?(params[:from])
       raise LoadError.new "#{params[:from]} not found"
-    elsif not params.has_key?(:to) or params[:to].nil?
+    elsif not params.has_key?(:to) || params[:to].nil?
       raise ArgumentError.new 'where should I save the generated pdf file?'
     else
       @from, @to = params[:from], params[:to]
+
       if params.has_key?(:except)
         @except = params[:except]
-        raise LoadError.new "#{@except} is not a valid blacklist yaml file" unless valid_blacklist?
+        raise LoadError.new "#{@except} is not a valid blacklist YAML file" unless valid_blacklist?
       end
+
       save
     end
   end
@@ -43,7 +45,7 @@ class ConvertToPDF
   def valid_blacklist?
     return false if not File.exists?(@except) or FileTest.directory?(@except)
     @blacklist = YAML.load(File.read(@except))
-    @blacklist.has_key?(:directories) and @blacklist.has_key?(:files)
+    @blacklist.has_key?(:directories) && @blacklist.has_key?(:files)
   end
 
   def in_directory_blacklist?(item_path)
@@ -64,19 +66,18 @@ class ConvertToPDF
     File.exists?(file) && FileTest.file?(file)
   end
 
-  def read_files(path=nil)
+  def read_files(path = nil)
     @files ||= []
-    path ||= @from
+    path   ||= @from
+
     Dir.foreach(path) do |item|
       item_path = "#{path}/#{item}"
 
-      if valid_directory?(item_path) and not ['.', '..'].include?(item)
-        unless in_directory_blacklist?(item_path)
-          read_files(item_path)
-        end
+      if valid_directory?(item_path) && !['.', '..'].include?(item)
+        read_files(item_path) unless in_directory_blacklist?(item_path)
       elsif valid_file?(item_path)
         unless in_file_blacklist?(item_path)
-          content = processing_file(item_path)
+          content = process_file(item_path)
           @files << [item_path, content]
         end
       end
@@ -84,11 +85,11 @@ class ConvertToPDF
     @files
   end
 
-  def processing_file(file)
+  def process_file(file)
     content = ''
-    File.open(file,'r') do |f|
+    File.open(file, 'r') do |f|
       f.each_line.with_index do |line_content, line_number|
-        # FIXME: please, refactore me!
+        # OPTIMIZE
         content << line_content.gsub(/</,'&lt;').gsub(/^/, "<color rgb='AAAAAA'>#{line_number + 1}</color>  ")
       end
     end
