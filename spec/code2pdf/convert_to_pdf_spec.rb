e481d3dc1c8f14c29225164dccbb7f254782b620
Lucas Caton <lucascaton@gmail.com>
2016-09-24 10:22:37 +1000

    Clean up specs

--- spec/code2pdf/convert_to_pdf_spec.rb
+++ spec/code2pdf/convert_to_pdf_spec.rb
@@ -3,30 +3,33 @@ require 'digest/md5'
 
 describe ConvertToPDF do
   describe '#pdf' do
-    it 'should create a PDF file containing all desired source code' do
+    it 'creates a PDF file containing all desired source code' do
       path      = 'spec/fixtures/hello_world'
       pdf       = 'spec/fixtures/hello_world.pdf'
       blacklist = 'spec/fixtures/hello_world/.code2pdf'
-      ConvertToPDF.new :from => path, :to => pdf, :except => blacklist
-      Digest::MD5.hexdigest(File.read(pdf)).should eq '619b483b7f175d915bd0bea65c2575f3'
-      File.delete pdf
+
+      ConvertToPDF.new from: path, to: pdf, except: blacklist
+      Digest::MD5.hexdigest(File.read(pdf)).should eq('a8fd2fe3b1fef17f6e13972ff1623ecb')
+      File.delete(pdf)
     end
 
-    it 'should verify if essential params are present' do
-      expect{ConvertToPDF.new(:foo => 'bar')}.to raise_error ArgumentError
+    it 'raises an error if required params are not present' do
+      expect { ConvertToPDF.new(foo: 'bar') }.to raise_error(ArgumentError)
     end
 
-    it 'should verify if specified path exists' do
+    it 'raises an error if path does not exist' do
       path = 'spec/fixtures/isto_non_existe_quevedo'
       pdf  = 'spec/fixtures/isto_non_existe_quevedo.pdf'
-      expect{ConvertToPDF.new(:from => path, :to => pdf)}.to raise_error LoadError
+
+      expect { ConvertToPDF.new(from: path, to: pdf) }.to raise_error(LoadError)
     end
 
-    it 'should verify if specified blacklist file is valid' do
+    it 'raises an error if blacklist file is not valid' do
       path      = 'spec/fixtures/hello_world'
       pdf       = 'spec/fixtures/hello_world.pdf'
       blacklist = 'spec/fixtures/purplelist.yml'
-      expect{ConvertToPDF.new :from => path, :to => pdf, :except => blacklist}.to raise_error LoadError
+
+      expect { ConvertToPDF.new from: path, to: pdf, except: blacklist }.to raise_error(LoadError)
     end
   end
 end
