f53a2461e8d6102dc207be410f0e3a060fd743b0
Lucas Caton <lucascaton@gmail.com>
2017-10-16 09:45:10 +1000

    Improve spec_helper settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
-$:.push File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'code2pdf'
+
+Dir['./spec/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+end
